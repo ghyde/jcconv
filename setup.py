612335f55a85913ca9229de0054538b7f46f3f56
Garrett Hyde <github@garretthyde.com>
2014-11-19 23:15:15 -0700

    Fix indentation.

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ import jcconv
 from jcconv import __version__, __license__, __author__
 
 if __name__ == '__main__':
-  from jcconv import jcconv_test
-  # run module test
-  loader = unittest.TestLoader()
-  result = unittest.TestResult()
-  suite  = loader.loadTestsFromModule(jcconv_test)
-  suite.run(result)
-  if not result.wasSuccessful():
-    print "unit tests have failed!"
-    print "aborted to make a source distribution"
-    sys.exit(1)
+    from jcconv import jcconv_test
+    # run module test
+    loader = unittest.TestLoader()
+    result = unittest.TestResult()
+    suite  = loader.loadTestsFromModule(jcconv_test)
+    suite.run(result)
+    if not result.wasSuccessful():
+        print "unit tests have failed!"
+        print "aborted to make a source distribution"
+        sys.exit(1)
 
-  # build distribution package
-  setup(
+    # build distribution package
+    setup(
     packages         = ('jcconv',),
     name             = 'jcconv',
     version          = __version__,
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     keywords         = 'japanese converter, hiragana, katakana, half-width kana',
     license          = __license__,
     classifiers      = ["Development Status :: 3 - Alpha",
-                        "Intended Audience :: Developers",
-                        "License :: OSI Approved :: MIT License",
-                        "Operating System :: POSIX",
-                        "Programming Language :: Python",
-                        "Topic :: Software Development :: Libraries :: Python Modules"]
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules"]
     )
